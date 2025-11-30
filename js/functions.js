8b94f0455db8ad34aadef6b9eeb14729165e3583
JuliyaEduardovna <juliya.eduardovna@gmail.com>
2025-11-30 19:11:31 +0700

    5.9. Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -36,3 +36,25 @@ function getNumbers(str) {
 
 getNumbers(2023);
 getNumbers('2025 год');
+
+/* 5.9. Функции возвращаются */
+
+const getMinutes = (time) => {
+  const [hours, minutes] = time.split(':');
+  return Number(hours) * 60 + Number(minutes);
+};
+
+const isWithinTimeRange = (workStart, workEnd, meetingStart, meetingDuration) => {
+  const workStartMinutes = getMinutes(workStart);
+  const workEndMinutes = getMinutes(workEnd);
+  const meetingStartMinutes = getMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+
+  return meetingStartMinutes >= workStartMinutes && meetingEndMinutes <= workEndMinutes;
+};
+
+isWithinTimeRange('08:00', '17:30', '14:00', 90); // true
+isWithinTimeRange('8:0', '10:0', '8:0', 120);     // true
+isWithinTimeRange('08:00', '14:30', '14:00', 90); // false
+isWithinTimeRange('14:00', '17:30', '08:0', 90);  // false
+isWithinTimeRange('8:00', '17:30', '08:00', 900); // false
