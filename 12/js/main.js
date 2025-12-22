c3cf191c7d0de55605bdaeac45d19980146c2ff0
Keks <keks@htmlacademy.ru>
2025-12-22 18:57:55 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/main.js
+++ 12/js/main.js
@@ -1,10 +1,6 @@
 import { generatePosts } from './data.js';
 import { renderThumbnail } from './render-thumbnails.js';
 import { initForm } from './form.js';
-import { initScale } from './scale.js';
-import { initEffects } from './effects.js';
 
 renderThumbnail(generatePosts());
 initForm();
-initScale();
-initEffects();
